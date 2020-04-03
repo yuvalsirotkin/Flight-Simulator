adc4a7da4f14ccf933d4fcfebb11e42e42af279a
hadargold <hadargold1@gmail.com>
2020-04-03 13:06:41 +0300

    0

--- FlightSimulatorApp/Model/NavigatorModel.cs
+++ FlightSimulatorApp/Model/NavigatorModel.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.ComponentModel;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace FlightSimulatorApp.Model
 {
     class NavigatorModel : INotifyPropertyChanged
     {
         private double throttle;
-        private double elavetor;
+        private double elevetor;
         private double rudder;
         private double aileron;
 
@@ -18,13 +21,22 @@ namespace FlightSimulatorApp.Model
 
         public double Elavetor
         {
-            get { return this.elavetor; }
+            get { return this.elevetor; }
             set
             {
-                this.elavetor = value;
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
-                e.Name = "elavetor";
-                e.Path = "/controls/flight/elevator";
+                this.elevetor = value;
+                if (value > 1)
+                {
+                    this.elevetor = 1;
+                }
+                if (value < -1)
+                {
+                    this.elevetor = -1;
+                }
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs("elevetor");
+                //e.Name = "elavetor";
+                //e.Path = "/controls/flight/elevator";
+                //e.Val = value;
                 NotifyPropertyChanged(e);
             }
         }
@@ -34,9 +46,10 @@ namespace FlightSimulatorApp.Model
             set
             {
                 this.throttle = value;
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
-                e.Name = "throttle";
-                e.Path = "/controls/engines/engine/throttle";
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs("throttle");
+                //e.Name = "throttle";
+                //e.Path = "/controls/engines/current-engine/throttle";
+                //e.Val = value;
                 NotifyPropertyChanged(e);
             }
         }
@@ -46,9 +59,10 @@ namespace FlightSimulatorApp.Model
             set
             {
                 this.aileron = value;
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
-                e.Name = "aileron";
-                e.Path = "/controls/flight/aileron";
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs("aileron");
+                //e.Name = "aileron";
+                //e.Path = "/controls/flight/aileron";
+                //e.Val = value;
                 NotifyPropertyChanged(e);
             }
         }
@@ -58,13 +72,22 @@ namespace FlightSimulatorApp.Model
             set
             {
                 this.rudder = value;
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
-                e.Name = "rudder";
-                e.Path = "/controls/flight/rudder";
+                if (value > 1)
+                {
+                    this.rudder = 1;
+                }
+                if (value < -1)
+                {
+                    this.rudder = -1;
+                }
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs("rudder");
+                //e.Name = "rudder";
+                //e.Path = "/controls/flight/rudder";
+                //e.Val = value;
                 NotifyPropertyChanged(e);
             }
         }
-
+        
         public void NotifyPropertyChanged(PropertyChangedEventArgs propName)
         {
             if (this.PropertyChanged != null)
@@ -72,30 +95,208 @@ namespace FlightSimulatorApp.Model
         }
 
 
-        TCPSet tcpSet;
+        TcpGetSet tcpClient;
         volatile Boolean stop;
 
-        public NavigatorModel(TCPSet tcpCLient, string ip, int port)
+      
+
+        public NavigatorModel(TcpGetSet tcpClient)
         {
-            this.tcpSet = tcpCLient;
-            tcpSet.connect(ip, port);
+            this.tcpClient = tcpClient;
             PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
             {
+                string path = "";
+                double val = 0;
+                switch (e.PropertyName)
+                {
+                    case "elevetor":
+                        path = "/controls/flight/elevator";
+                        val = Elavetor;
+                        if (val != 0)
+                        {
+                            Console.WriteLine("el not 0");
+                        }
+                        break;
+                    case "throttle":
+                        path = "/controls/engines/current-engine/throttle";
+                        val = Throttle;
+                        if (val == 1)
+                        {
+                            Console.WriteLine("th is 1");
+                        }
+                        break;
+                    case "aileron":
+                        path = "/controls/flight/aileron";
+                        val = Aileron;
+                        if (val != 0)
+                        {
+                            Console.WriteLine("ai not 0");
+                        }
+                        break;
+                    case "rudder":
+                        path = "/controls/flight/rudder";
+                        val = Rudder;
+                        if (val != 0)
+                        {
+                            Console.WriteLine("ru not 0");
+                        }
+                        break;
+
+
+                }
                 // set the property in the simulator
-                tcpCLient.write("set" + e.Path + e.Val + "\n");
+                tcpClient.write("set " + path + " " + val + "\n");
+                string[] splittedData = System.Text.RegularExpressions.Regex.Split(tcpClient.read(), "\n");
+                Console.WriteLine(splittedData[0]);
+
 
             };
             stop = false;
         }
+
+        
+
+        //public NavigatorModel(TCPSet tcpCLient)
+        //{
+        //    this.tcpClient = tcpCLient;
+        //    PropertyChanged += (object sender, System.ComponentModel.PropertyChangedEventArgs e) =>
+        //    {
+        //        Model.PropertyChangedEventArgs e1 = Model.PropertyChangedEventArgs.converPropertyChangedEventArgs(e);
+        //        // set the property in the simulator
+        //        tcpCLient.write("set" + e1.Path + e1.Val + "\n");
+
+        //    };
+        //    stop = false;
+        //}
+
         public void connect(string ip, int port)
         {
-            tcpSet.connect(ip, port);
+            tcpClient.connect(ip, port);
         }
         public void disconnect()
         {
             stop = true;
-            tcpSet.disconnect();
+            tcpClient.disconnect();
         }
 
     }
 }
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using System.Net.Sockets;
+
+//namespace FlightSimulatorApp.Model
+//{
+//    class NavigatorModel : INotifyPropertyChanged
+//    {
+//        private double throttle;
+//        private double elavetor;
+//        private double rudder;
+//        private double aileron;
+
+//        public event PropertyChangedEventHandler PropertyChanged;
+
+//        public double Elavetor
+//        {
+//            get { return this.elavetor; }
+//            set
+//            {
+//                this.elavetor = value;
+//                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
+//                e.Name = "elavetor";
+//                e.Path = "/controls/flight/elevator";
+//                NotifyPropertyChanged(e);
+//            }
+//        }
+//        public double Throttle
+//        {
+//            get { return this.throttle; }
+//            set
+//            {
+//                this.throttle = value;
+//                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
+//                e.Name = "throttle";
+//                e.Path = "/controls/engines/engine/throttle";
+//                NotifyPropertyChanged(e);
+//            }
+//        }
+//        public double Aileron
+//        {
+//            get { return this.aileron; }
+//            set
+//            {
+//                this.aileron = value;
+//                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
+//                e.Name = "aileron";
+//                e.Path = "/controls/flight/aileron";
+//                NotifyPropertyChanged(e);
+//            }
+//        }
+//        public double Rudder
+//        {
+//            get { return this.rudder; }
+//            set
+//            {
+//                this.rudder = value;
+//                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
+//                e.Name = "rudder";
+//                e.Path = "/controls/flight/rudder";
+//                NotifyPropertyChanged(e);
+//            }
+//        }
+
+//        public void NotifyPropertyChanged(PropertyChangedEventArgs propName)
+//        {
+//            if (this.PropertyChanged != null)
+//                this.PropertyChanged(this, propName);
+//        }
+
+
+//        TcpGetSet tcpClient;
+//        volatile Boolean stop;
+
+//        public NavigatorModel(TcpGetSet tcpClient)
+//        {
+//            this.tcpClient = tcpClient;
+//            PropertyChanged += (object sender, System.ComponentModel.PropertyChangedEventArgs e) =>
+//            {
+//                Model.PropertyChangedEventArgs e1 = Model.PropertyChangedEventArgs.converPropertyChangedEventArgs(e);
+//                // set the property in the simulator
+//                tcpClient.write("set" + e1.Path + e1.Val + "\n");
+
+//            };
+//            stop = false;
+//        }
+
+//        //public NavigatorModel(TCPSet tcpCLient)
+//        //{
+//        //    this.tcpClient = tcpCLient;
+//        //    PropertyChanged += (object sender, System.ComponentModel.PropertyChangedEventArgs e) =>
+//        //    {
+//        //        Model.PropertyChangedEventArgs e1 = Model.PropertyChangedEventArgs.converPropertyChangedEventArgs(e);
+//        //        // set the property in the simulator
+//        //        tcpCLient.write("set" + e1.Path + e1.Val + "\n");
+
+//        //    };
+//        //    stop = false;
+//        //}
+
+//        public void connect(string ip, int port)
+//        {
+//            tcpClient.connect(ip, port);
+//        }
+//        public void disconnect()
+//        {
+//            stop = true;
+//            tcpClient.disconnect();
+//        }
+
+//    }
+//}
+
+
--- FlightSimulatorApp/ViewModel/SimViewModel.cs
+++ FlightSimulatorApp/ViewModel/SimViewModel.cs
@@ -4,54 +4,198 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FlightSimulatorApp.Model;
+using System.ComponentModel;
+using Microsoft.Maps.MapControl.WPF;
 
 namespace FlightSimulatorApp.ViewModel
 {
-    class SimViewModel
+    class SimViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         private MapAndDashboardModel mapAndDashboardModel;
         private NavigatorModel navigatorModel;
 
-        private double throttle;
+        /*
         private double elavetor;
         private double rudder;
-        private double aileron;
+        private double aileron;*/
+        //remove it
+        private double throttle;
+        //until here
+        private double headingDeg;
+        private double verticalSpeed;
+        private double airSpeed;
+        private double roll;
+        private double altitude;
+        private double altimeter;
+        private double pitch;
+        private double groundSpeed;
+        private Location location;
+        private double latitude = 50;
+        private double longitude = 10;
+
         //properties
         // changes in the properties (from the view) will change the navigator model
-        public double Elavetor
+        public double VM_Elavetor
         {
-            get { return this.elavetor; }
+            /*get { return this.elavetor; }*/
             set
             {
-                this.elavetor = value;
+                /*this.elavetor = value;*/
                 navigatorModel.Elavetor = value;
             }
         }
-        public double Throttle
+        public double VM_Throttle
         {
             get { return this.throttle; }
             set
             {
+                // should delete it
                 this.throttle = value;
+                //until here
+                
                 navigatorModel.Throttle = value;
             }
         }
-        public double Aileron
+        public double VM_Aileron
         {
-            get { return this.aileron; }
+            /*get { return this.aileron; }*/
             set
             {
-                this.aileron = value;
+                /*this.aileron = value;*/
                 navigatorModel.Aileron = value;
             }
         }
-        public double Rudder
+        public double VM_Rudder
         {
-            get { return this.rudder; }
+            /*get { return this.rudder; }*/
             set
             {
-                this.rudder = value;
+                /*this.rudder = value;*/
                 navigatorModel.Rudder = value;
+            }
+        }
+
+        public double VM_HeadingDeg
+        {
+            get {
+                Console.WriteLine("get the heading deg is");
+                return this.headingDeg;
+            }
+            set
+            {
+                this.headingDeg = value;
+                Console.Write("the heading deg is");
+                Console.WriteLine(this.headingDeg);
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_HeadingDeg"));
+            }
+        }
+
+
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+
+
+        public double VM_VerticalSpeed
+        {
+            get { return this.verticalSpeed; }
+            set
+            {
+                this.verticalSpeed = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_VerticalSpeed"));
+            }
+        }
+        public double VM_GroundSpeed
+        {
+            get { return this.groundSpeed; }
+            set
+            {
+                this.groundSpeed = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_GroundSpeed"));
+            }
+        }
+        public double VM_Airspeed
+        {
+            get { return this.airSpeed; }
+            set
+            {
+                this.airSpeed = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_Airspeed"));
+            }
+        }
+        public double VM_Altitude
+        {
+            get { return this.altitude; }
+            set
+            {
+                this.altitude = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_Altitude"));
+            }
+        }
+        public double VM_Roll
+        {
+            get { return this.roll; }
+            set
+            {
+                this.roll = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_Roll"));
+            }
+        }
+
+        public double VM_Pitch
+        {
+            get { return this.pitch; }
+            set
+            {
+                this.pitch = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_Pitch"));
+            }
+        }
+
+        public double VM_Altimeter
+        {
+            get { return this.altimeter; }
+            set
+            {
+                this.altimeter = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_Altimeter"));
+            }
+        }
+
+        public double VM_Latitude
+        {
+            get { return this.latitude; }
+            set
+            {
+                this.latitude = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_Latitude"));
+            }
+        }
+
+        public double VM_Longitude
+        {
+            get { return this.longitude; }
+            set
+            {
+                this.longitude = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_Longitude"));
+            }
+        }
+
+        public Location VM_Location
+        {
+            get
+            {
+                return new Location(this.latitude, this.longitude);
+            }
+            set
+            {
+                OnPropertyChanged(new PropertyChangedEventArgs("VM_Location"));
             }
         }
 
@@ -61,22 +205,92 @@ namespace FlightSimulatorApp.ViewModel
             this.mapAndDashboardModel = mapAndDashboardModel;
             // when proprety changed in the model- it will notify the VM also
             // use the map and dashboard model
-            mapAndDashboardModel.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e) {
-                NotifyPropertyChanged("VM_" + e.Name);
+            mapAndDashboardModel.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyPropertyChanged(e.PropertyName);
             };
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // the propertyChanged should contain function that will change the view when the vm changed
         public void NotifyPropertyChanged(string propName)
         {
-            if (this.PropertyChanged != null)
+            switch (propName)
             {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs();
-                e.Name = propName;
-                this.PropertyChanged(this, e);
+                case "HeadingDeg":
+                    this.VM_HeadingDeg = MapAndDashboardModel.HeadingDeg;
+                    break;
+                case "VerticalSpeed": 
+                    this.VM_VerticalSpeed = MapAndDashboardModel.VerticalSpeed;
+                    break;
+                case "GroundSpeed":
+                    this.VM_GroundSpeed = MapAndDashboardModel.GroundSpeed;
+                    break;
+                case "AirSpeed":
+                    this.VM_Airspeed = MapAndDashboardModel.Airspeed;
+                    break;
+                case "Altitude":
+                    this.VM_Altitude = MapAndDashboardModel.Altitude;
+                    break;
+                case "Roll":
+                    this.VM_Roll = MapAndDashboardModel.Roll;
+                    break;
+                case "Pitch":
+                    this.VM_Pitch = MapAndDashboardModel.Pitch;
+                    break;
+                case "Altimeter":
+                    this.VM_Altimeter = MapAndDashboardModel.Altimeter;
+                    break;
+                case "Latitude":
+                    this.VM_Latitude = MapAndDashboardModel.Latitude;
+                    //this.location.Latitude = MapAndDashboardModel.Latitude;
+                    break;
+                case "Longitude":
+                    this.VM_Longitude = MapAndDashboardModel.Longitude;
+                    //this.location.Longitude = MapAndDashboardModel.Longitude;
+                    this.VM_Location = new Location (this.latitude, this.longitude);
+                    Console.Write("the latitude is");
+                    Console.WriteLine(this.latitude);
+                    break;
+
             }
         }
+
+        public string VM_LongitudeMsg
+        {
+            get
+            {
+                if (MapAndDashboardModel.Longitude == 180 || MapAndDashboardModel.Longitude == -180)
+                {
+                    return "Invalid Coordinate in Longitude";
+                }
+                return " ";
+            }
+        }
+        public string VM_LatitudeMsg
+        {
+            get
+            {
+                if (MapAndDashboardModel.Latitude == 90 || MapAndDashboardModel.Latitude == -90)
+                {
+                    return "Invalid Coordinate in Latitude";
+                }
+                return " ";
+            }
+        }
+
+
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //// the propertyChanged should contain function that will change the view when the vm changed
+        //public void NotifyPropertyChanged(PropertyChangedEventArgs property)
+        //{
+        //    if (this.PropertyChanged != null)
+        //    {
+        //        Model.PropertyChangedEventArgs e = new Model.PropertyChangedEventArgs();
+        //        e.Name = "VM_"+ property.Name;
+        //        e.Val = property.Val;
+        //        this.PropertyChanged(this, e);
+        //    }
+        //}
     }
 }
