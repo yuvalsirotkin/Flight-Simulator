df1442976c0a250e85c681df70ccc06dbec1b8ef
hadargold <hadargold1@gmail.com>
2020-04-06 14:21:45 +0300

    X is working

--- FlightSimulatorApp/MainWindow.xaml.cs
+++ FlightSimulatorApp/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+
 namespace FlightSimulatorApp
 {
     /// <summary>
@@ -19,9 +24,21 @@ namespace FlightSimulatorApp
     /// </summary>
     public partial class MainWindow : NavigationWindow
     {
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NavigationWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
         }
     }
 }
